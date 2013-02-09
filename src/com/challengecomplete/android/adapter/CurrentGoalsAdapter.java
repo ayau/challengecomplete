7a7e244a24cbb9b63f8d906f00a831bc84df7d60
Alex Yau <ayau@sas.upenn.edu>
2013-02-09 01:23:46 -0500

    decreased number of request and updated adapter when loading svg

--- src/com/challengecomplete/android/adapter/CurrentGoalsAdapter.java
+++ src/com/challengecomplete/android/adapter/CurrentGoalsAdapter.java
@@ -1,12 +1,8 @@
 package com.challengecomplete.android.adapter;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.support.v4.widget.CursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,23 +14,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.challengecomplete.android.R;
-import com.challengecomplete.android.service.HttpCaller;
 import com.challengecomplete.android.utils.BitmapCache;
-import com.challengecomplete.android.utils.Media;
-import com.larvalabs.svgandroid.SVG;
-import com.larvalabs.svgandroid.SVGParser;
+import com.challengecomplete.android.utils.SvgLoader;
 
 public class CurrentGoalsAdapter extends CursorAdapter {
 	private BitmapCache mBitmapCache;
+	private SvgLoader svgLoader;
 	
 	public CurrentGoalsAdapter(Context context, Cursor c) {
 		super(context, c, 0);
 		mBitmapCache = new BitmapCache();
+		svgLoader = new SvgLoader(context, this, mBitmapCache);
 	}
-
+	
 	@Override
 	public void bindView(View view, Context context, Cursor c) {
-		 int id = c.getInt(0);
+		int id = c.getInt(0);
 		String name = c.getString(1);
 		String badge = c.getString(8);
 		String color = c.getString(9);
@@ -43,16 +38,12 @@ public class CurrentGoalsAdapter extends CursorAdapter {
 		holder.name.setText(name);
 		holder.badge.setTag(id);
 		
-		if (name.equals("guitar"))
-		Log.i("BADGE", badge);
-		
 		// Check cache first
 		Bitmap bitmap = mBitmapCache.getBitmapFromMemCache(id + "");
-		
+//		Log.i("HERE", id + "");
 		if (bitmap == null){
 			holder.badge.setImageResource(R.drawable.ic_launcher);
-			BitmapWorkerTask task = new BitmapWorkerTask(id, holder.badge, badge, color);
-	        task.execute(id);
+			svgLoader.getSvg(id, badge, color);
 		} else {
 			holder.badge.setImageBitmap(bitmap);
 		}
@@ -67,58 +58,18 @@ public class CurrentGoalsAdapter extends CursorAdapter {
 		});
 	}
 	
-	class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
-	    private ImageView imageView;
-	    private String badge;
-	    private String color;
-	    private int id;
-	    
-		public BitmapWorkerTask(int id, ImageView imageView, String badge, String color) {
-			this.id = id;
-			this.imageView = imageView;
-			this.badge = badge;
-			this.color = color;
-		}
-
-		// Decode image in background.
-	    @Override
-	    protected Bitmap doInBackground(Integer... params) {
-//	    	Log.i("BADGE IS", badge);
-	    	String svgString = HttpCaller.getRequest(imageView.getContext(), badge);
-	    	
-	    	String svgColor = badge.substring(badge.length() - 6);
-	    	
-	    	svgString = svgString.replaceAll("fill=\"#000000\"", "");
-	    	svgString = svgString.replaceAll("<polygon", "<polygon fill=\"#" + svgColor + "\"");
-	    	svgString = svgString.replaceAll("<path", "<path fill=\"#" + svgColor + "\"");
-	    	svgString = svgString.replaceAll("<rect", "<rect fill=\"#" + svgColor + "\"");
-	    	
-	    	InputStream is = new ByteArrayInputStream(svgString.getBytes());
-			SVG svg = SVGParser.getSVGFromInputStream(is);
-			Bitmap bitmap = Media.getBadgeBitmap(imageView.getContext(), svg.createPictureDrawable(), color);
-			mBitmapCache.addBitmapToMemoryCache(params[0] + "", bitmap);
-	    	
-			return bitmap;
-	    }
-	    
-	    @Override
-	    protected void onPostExecute(Bitmap result) {
-	    	if (((Integer) imageView.getTag()) == id)
-	    		imageView.setImageBitmap(result);
-	     }
-	}
-
 	@Override
 	public View newView(Context context, Cursor c, ViewGroup parent) {
-		 LayoutInflater inflater = LayoutInflater.from(context);
-		 View v = inflater.inflate(R.layout.list_item_goal, parent, false);
-		 ViewHolder holder = new ViewHolder();
-		 holder.name = (TextView) v.findViewById(R.id.goal_name);
-		 holder.badge = (ImageView) v.findViewById(R.id.goal_badge);
-		 holder.square = (ImageView) v.findViewById(R.id.square);
-		 v.setTag(holder);
-		 bindView(v, context, c);
-		 return v;
+		Log.i("NEW VIEW", "NEW");
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View v = inflater.inflate(R.layout.list_item_goal, parent, false);
+		ViewHolder holder = new ViewHolder();
+		holder.name = (TextView) v.findViewById(R.id.goal_name);
+		holder.badge = (ImageView) v.findViewById(R.id.goal_badge);
+		holder.square = (ImageView) v.findViewById(R.id.square);
+		v.setTag(holder);
+//		bindView(v, context, c);
+		return v;
 	}
 
 	class ViewHolder {
--- src/com/challengecomplete/android/utils/SvgLoader.java
+++ src/com/challengecomplete/android/utils/SvgLoader.java
@@ -1,15 +1,78 @@
 package com.challengecomplete.android.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.widget.BaseAdapter;
+
+import com.challengecomplete.android.service.HttpCaller;
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
+
 public class SvgLoader {
 	
-    /**
-     * The default maximum number of active tasks.
-     */
-    public static final int DEFAULT_TASK_LIMIT = 3;
-    
-    public SvgLoader(){
-    	
-    }
-    
+//    public static final int DEFAULT_TASK_LIMIT = 3;
+	private BitmapCache mBitmapCache;  
+	private HashMap<Integer, BitmapWorkerTask> workers;
+	private BaseAdapter adapter;
+	private Context context;
+	
+	public SvgLoader(Context context, BaseAdapter adapter, BitmapCache mBitmapCache) {
+		this.mBitmapCache = mBitmapCache;
+		workers = new HashMap<Integer, BitmapWorkerTask>();
+		this.adapter = adapter;
+		this.context = context;
+	}
+	
+	public void getSvg(int id, String badge, String color){
+		if (!workers.containsKey(id)){
+			BitmapWorkerTask task = new BitmapWorkerTask(id, badge, color);
+	        workers.put(id, task);
+			task.execute(id);
+		}
+	}
+
+	class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+	    private String badge;
+	    private String color;
+	    private int id;
+	    
+		public BitmapWorkerTask(int id, String badge, String color) {
+			this.id = id;
+			this.badge = badge;
+			this.color = color;
+		}
+
+		// Decode image in background.
+	    @Override
+	    protected Bitmap doInBackground(Integer... params) {
+	    	String svgString = HttpCaller.getRequest(context, badge);
+	    	
+	    	String svgColor = badge.substring(badge.length() - 6);
+	    	
+	    	svgString = svgString.replaceAll("fill=\"#000000\"", "");
+	    	svgString = svgString.replaceAll("<polygon", "<polygon fill=\"#" + svgColor + "\"");
+	    	svgString = svgString.replaceAll("<path", "<path fill=\"#" + svgColor + "\"");
+	    	svgString = svgString.replaceAll("<rect", "<rect fill=\"#" + svgColor + "\"");
+	    	
+	    	InputStream is = new ByteArrayInputStream(svgString.getBytes());
+			SVG svg = SVGParser.getSVGFromInputStream(is);
+			Bitmap bitmap = Media.getBadgeBitmap(context, svg.createPictureDrawable(), color);
+			mBitmapCache.addBitmapToMemoryCache(params[0] + "", bitmap);
+	    	
+			return bitmap;
+	    }
+	    
+	    @Override
+	    protected void onPostExecute(Bitmap result) {
+	    	workers.remove(id);
+	    	adapter.notifyDataSetChanged();
+	     }
+	}
+
     
 }
